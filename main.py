ea5f44a5790f1528fa0427595219060fd5711d25
Jeroen Esseveld <jeroenesseveld@gmail.com>
2019-03-26 13:59:20 +0100

    bla

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import json
 from typing import Dict, List, Tuple
 import requests
 
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.model_selection import cross_validate
+from sklearn import datasets
 
 def extract_features(dataset: List[Dict]) -> List[Dict]:
     result = []
@@ -73,7 +77,17 @@ def main() -> None:
     instanceA1, truthA1 = parse("datasetA1/instances.jsonl", "datasetA1/truth.jsonl")
     instanceA2, truthA2 = parse("datasetA2/instances.jsonl", "datasetA2/truth.jsonl")
     clickbaitB, nonclickbaitB = parse("datasetB/clickbait_data.jsonl", "datasetB/non_clickbait_data.jsonl")
-    featuresA1 = extract_features(instanceA1)
+
+    clf = RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)
+    iris = datasets.load_iris()
+    #TODO insert predictions here!
+    X, y = iris.data, iris.target
+    clf.fit(X, y)
+    cv_results = cross_validate(clf, X, y, cv=5, return_train_score = False)
+    print(cv_results['test_score'])
+
+
+    pass
 
 
 def parse(file1: str, file2: str) -> Tuple[List[Dict], List[Dict]]:
